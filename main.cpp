b8a4cb97beeeede4acdc9fb70c489ff402a071f9
DiogoBCLima <diogobclima@gmail.com>
2021-09-15 15:03:58 -0300

    v.01
    primeira tentativa de compilar um programa que inclui python.h

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include </Usuarios/diogo/anaconda3/include/python.h>
+
+using namespace std;
+
+int main (int argc, char **argv)
+{
+    cout << "Teste" << endl;
+
+    return 0;
+}
